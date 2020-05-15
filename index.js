4322c37bf92b87edc82957b582ad8da2b66f13eb
bobajack123 <45500930+bobajack123@users.noreply.github.com>
2020-05-15 12:42:16 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+window.onload = function() {
+        document.getElementById("JIP").innerHTML = "welcome to Jack images";
+    }
+    // doesnt work yet
+    // "If you're going through hell, keep going." - Winston Churchill
