2b3f560d6678cd36aea3749ac5e99f791ed61a7e
bobajack123 <45500930+bobajack123@users.noreply.github.com>
2020-05-18 11:04:26 +0100

    Add files via upload
    version 2.0.1

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 window.onload = function() {
-        document.getElementById("JIP").innerHTML = "welcome to Jack images";
-    }
-    // doesnt work yet
-    // "If you're going through hell, keep going." - Winston Churchill
+    document.getElementById("JIP").innerHTML = "welcome to Jack images";
+}
+window.onload = function() {
+    window.alert("this is still in development and will be for the for a while.")
+}
+window.onload = function() {
+    this.console.log("why are you looking in here HUH if your getting errors dm me on discord @jAcK#8161")
+}
+
+// doesnt work yet
+// "If you're going through hell, keep going." - Winston Churchill
