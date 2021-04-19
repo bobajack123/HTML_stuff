90c3770d293db6a34c17e7d45ca7a03536964cbe
bobajack123 <45500930+bobajack123@users.noreply.github.com>
2021-04-19 12:46:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-window.onload = function() {
-    document.getElementById("JIP").innerHTML = "welcome to Jack images";
-}
-window.onload = function() {
-    window.alert("this is still in development and will be for the for a while.")
-}
-window.onload = function() {
-    this.console.log("why are you looking in here HUH if your getting errors dm me on discord @jAcK#8161")
-}
-window.onload = function() {
-        document.getElementById("APISTAT").innerHTML = "/api/get-stats?s=1&f=1 "
-    }
+// window.onload = function() {
+//    document.getElementById("JIP").innerHTML = "welcome to Jack images";
+//}
+//window.onload = function() {
+  //  window.alert("this is still in development and will be for the for a while.")
+//}
+//window.onload = function() {
+  //  this.console.log("why are you looking in here HUH if your getting errors dm me on discord @jAcK#8161")
+//}
+//window.onload = function() {
+  //      document.getElementById("APISTAT").innerHTML = "/api/get-stats?s=1&f=1 "
+    //}
     // gets information from the api that can see the amount of files on there
 
 // doesnt work yet
